ee2754618f9639b1fee9f68feda39b8a9aa31660
Lehua <andrea.albani1212@gmail.com>
2024-05-07 11:25:49 +0200

    generated administration's server and client class

---
+++ src/main/java/server/AdministrationServer.java
@@ -0,0 +1,32 @@
+package server;
+
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+
+public class AdministrationServer {
+
+    private final static String HOST = "localhost";
+    private final static int PORT = 8080;
+
+    public static void main(String[] args) {
+        try {
+
+            HttpServer administrationServer = HttpServerFactory.create("http://" + HOST + ":" + PORT + "/");
+            administrationServer.start();
+
+            System.out.println("Server is running on: http://" + HOST + ":" + PORT);
+            System.in.read();
+            System.out.println("Stopping server");
+            administrationServer.stop(0);
+            System.out.println("Server stopped");
+
+        } catch (IOException e) {
+
+            System.out.println("Error: " + e.getMessage());
+            throw new RuntimeException(e);
+
+        }
+    }
+}
